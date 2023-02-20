af97a6dfe2fb21134ae69e13eacaa322336c2a10
T-Stolf <thomsstolf@gmail.com>
2023-02-20 17:44:05 -0500

    Integrating chat storage endpoints and allocation of verification to seperate module

--- Client/src/utils/APIRoutes.js
+++ Client/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
 const host = "http://localhost:4000";
-export const registerRoute = `${host}/api/admin/newAdmin`;
-export const loginRoute = `${host}/api/admin/Login`;
+export const registerRoute = `${host}/api/admin/register`;
+export const loginRoute = `${host}/api/admin/login`;
 export const anonymousRoute = `${host}/api/anonymous`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`; //???
 export const allUsersRoute = `${host}/api/auth/allusers`; //???
