f7128e3ee4a49e47782c27871ef592f6b001c715
unknown <ngevangelion.03@gmail.com>
2023-02-20 21:08:04 -0500

    Fixing mistakes, need to wipe localStorage if anon

--- Client/src/pages/Chat.jsx
+++ Client/src/pages/Chat.jsx
@@ -8,22 +8,26 @@ function Chat(){
     const [username, setUsername] = useState("")
     const [room,setRoom] = useState("");
     const [showChat, setShowChat] = useState(false);
-    //Get the "data" from localStorage using key `HelpDeskAppV1`
-    var str = localStorage.getItem(`HelpDeskAppV1`);
-    //Gives a string in base64, only need info between 2 periods
-    var subStr = str.substring(
-      str.indexOf('.') + 1, 
-      str.lastIndexOf('.')
-    );
-    //Decode from base64
-    var dec = atob(subStr);
-    //Get substring between second instance of ':' and first instance of ','
-    var newStr = dec.substring(
-      str.indexOf(':', 10) + 1, 
-      str.indexOf(',')
-    );
-    //Check
-    console.log(newStr);
+    var val = "";
+    if (localStorage.getItem(`HelpDeskAppV1`) != null) {
+      //Get the "data" from localStorage using key `HelpDeskAppV1`
+      var str = localStorage.getItem(`HelpDeskAppV1`);
+      //Gives a string in base64, only need info between 2 periods
+      var subStr = str.substring(
+        str.indexOf('.') + 1, 
+        str.lastIndexOf('.')
+      );
+      //Decode from base64
+      var dec = atob(subStr);
+      //Get substring between second instance of ':' and first instance of ','
+      var newStr = dec.substring(
+        dec.indexOf(':"', 10) + 2, 
+        dec.indexOf('",')
+      );
+      //Check string
+      console.log(newStr);
+      val = newStr;
+    }
 
     const joinRoom = () => {
       if (username !== "" && room !== ""){
@@ -38,7 +42,7 @@ function Chat(){
         {!showChat ? (
       <div className="chatContainer">
        <h3> Join A Chat</h3>
-        <input type="text" placeholder="John ..." onChange={(event) => {setUsername(event.target.value);}}/>
+        <input type="text" placeholder="John ..." value={val} onChange={(event) => {setUsername(event.target.value);}}/>
         <input type="text" placeholder="Room ID ..." onChange={(event) => {setRoom(event.target.value);}}/> 
         <button onClick={joinRoom}> Join A Room</button>
       </div>
